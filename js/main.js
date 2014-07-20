ed9a71a4d76dc91c7ff359b9b40ea2d6c3262455
ElenaPilipchak <elena.pilipchak@mail.ru>
2014-07-20 09:07:49 -1200

    update table input

--- js/main.js
+++ js/main.js
@@ -3,5 +3,19 @@ $(function() {
 });
 
 function saveFormData() {
-    alert("Это функция для сохранения")
+}
+$function()
+{
+    var a="Name"*obj.jan.value;
+    var b="".feb.value;
+    var c="Surname"*obj.mar.value;
+    var d="E-mail"*obj.apr.value;
+    var e="Password"*obj.may.value;
+    var f="Repeat password"*obj.jun.value;
+    var g="Date of birth"*obj.jul.value;
+    var h="Phone"*obj.aug.value;
+    var i="Country, town, region"*obj.sep.value;
+    var j="Address"*obj.oct.value;
+    var s=a+b+c+d+e+f+g+h+i+j;
+    obj.polya.value=s;
 }
