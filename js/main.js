eb87c21e44857e72597e94cff7fb6d177734928b
EugeneZakharov <evgeniykpi@gmail.com>
2014-07-21 10:41:14 +0300

    refactoring html and js

--- js/main.js
+++ js/main.js
@@ -3,19 +3,10 @@ $(function() {
 });
 
 function saveFormData() {
-}
-$function()
-{
-    var a="Name"*obj.jan.value;
-    var b="".feb.value;
-    var c="Surname"*obj.mar.value;
-    var d="E-mail"*obj.apr.value;
-    var e="Password"*obj.may.value;
-    var f="Repeat password"*obj.jun.value;
-    var g="Date of birth"*obj.jul.value;
-    var h="Phone"*obj.aug.value;
-    var i="Country, town, region"*obj.sep.value;
-    var j="Address"*obj.oct.value;
-    var s=a+b+c+d+e+f+g+h+i+j;
-    obj.polya.value=s;
+    var inputsArray = $("input")
+    var resultString = ""
+    for (var i=0; i<inputsArray.length; i++){
+        resultString = resultString + inputsArray[i].value + "  "
+    }
+    $("textarea")[0].value = resultString
 }
